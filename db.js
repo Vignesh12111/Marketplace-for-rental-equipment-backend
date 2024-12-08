4e71fe6792d53e3458838a18086ea563a55df739
Ratheesh R <145856381+Vignesh12111@users.noreply.github.com>
2024-12-09 00:14:19 +0530

    backend code push

---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+function connectDB(){
+    try {
+      mongoose.connect(process.env.MONGO_URL, {
+        useUnifiedTopology: true, 
+        useNewUrlParser: true,
+        serverSelectionTimeoutMS: 5000,
+
+      })
+      .then(() => console.log('MongoDB Connected Successfully'))
+      .catch((err) => console.error('MongoDB Connection Error:', err));
+    } catch (error) {
+      console.error('Connection Error:', error);
+    }
+  }
+
+
+connectDB()
+
+module.exports = mongoose
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Database connection
+const dbConnection = require('./db');
+
+// Middleware to parse JSON
+http://localhost:5000/api/users/register
+app.use(express.json());
+
+// Load environment variables
+dotenv.config();
+
+// Enable CORS
+app.use(cors());
+
+// API Routes
+app.use('/api/cars/', require('./routes/carsRoute'));
+app.use('/api/users/', require('./routes/usersRoute'));
+app.use('/api/bookings/', require('./routes/bookingsRoute'));
+
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use('/', express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
+
+// Base route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the server
+app.listen(port, () => console.log(`Node JS Server Started on Port ${port}`));
